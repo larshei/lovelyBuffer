0ea20beb263b7600b56ce12106ec8ec455ec6a04
Lars Heinrichs <lars.heinrichs@web.de>
2020-05-13 15:04:08 +0200

    Add prototypes for buffer type functions to header file

--- buf_buffer.h
+++ buf_buffer.h
@@ -41,6 +41,11 @@ unsigned int buf_return_count();
 
 
 // === BUFFER CONTENT MANAGEMENT ===
+uint8_t buf_reset_read(buf_buffer_t buffer);
+uint8_t buf_get_buffer_mode(buf_buffer_t buffer);
+uint8_t buf_fifo_buffer_mode(buf_buffer_t buffer);
+uint8_t buf_ring_buffer_mode(buf_buffer_t buffer);
+
 uint8_t buf_add_element (buf_buffer_t buffer, DATA_TYPE element);
 DATA_TYPE* buf_read_element (buf_buffer_t buffer);
 uint8_t buf_is_empty(buf_buffer_t buffer);
