e92d230cfbc45c6714af627dd1cbe133c7a23426
Lars Heinrichs <lars.heinrichs@web.de>
2020-06-16 16:34:55 +0200

    Changed config to default config that includes a user config file. Moved all default values to default config file.

--- buf_default_config.h
+++ buf_default_config.h
@@ -1,7 +1,14 @@
 #ifndef __BUF_BUFFER_CONFIG_H
 #define __BUF_BUFFER_CONFIG_H
 
-#define DATA_TYPE           uint32_t
+#include "../buf_config.h"
+
+#ifndef BUF_BUFFER_COUNT
 #define BUF_BUFFER_COUNT     4
+#endif
+
+#ifndef DATA_TYPE
+#define DATA_TYPE           uint32_t
+#endif
 
 #endif // __BUF_BUFFER_CONFIG_H
